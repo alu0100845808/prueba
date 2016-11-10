d1041bd0ad0f66ee4f3f2a7ddabf7144840b6dcc
Alejandro Carrillo <alu0100845808@ull.edu.es>
2016-11-10 17:58:47 +0000

    Dieta creada

---
+++ Dieta/lib/Dieta/Dieta.rb
@@ -0,0 +1,13 @@
+class Dieta
+    attr_accessor :titulo,:porcmenu,:desc_platos,:por_platos,:gr_platos,:vct,:porcprot,:porcgrasas,:porchidratos
+    def initialize(titulo,porcmenu,desc_platos,por_platos,gr_platos,vct,porcprot,porcgrasas,porchidratos)
+        @titulo=titulo
+        @porcmenu=porcmenu
+        @desc_platos=desc_platos
+        @por_platos=por_platos
+        @gr_platos=gr_platos
+        @vct=vct
+        @porcprot=porcprot
+        @porcgrasas=porcgrasas
+        @porchidratos=porchidratos
+    end
