8607a893cf1fa9890f593e513562b69f66e0bef5
Alejandro Carrillo <alu0100845808@ull.edu.es>
2016-11-10 17:52:58 +0000

    Pruebas acabadas

--- Dieta/spec/Dieta_spec.rb
+++ Dieta/spec/Dieta_spec.rb
@@ -5,9 +5,83 @@ require '/home/ubuntu/workspace/p7/Dieta/lib/LList'
   describe Dieta do
     before :each do
         @d1 = Dieta.new('ALMUERZO', 30,  ['Macarrones con salsa de tomate y queso parmesano', 'Escalope de ternera', 'Ensalada básica con zanahoria rallada','Mandarina', 'Pan de trigo integral'], ['1 1/2 cucharón', '1 bistec mediano', '1 guarnición', '1 grande', '1 rodaja'], [200, 100, 120, 180, 20], 785.9, 19, 34, 47)
-        @d2 = Dieta.new('ALMUERZO', 30,  ['Macarrones con salsa de tomate y queso parmesano', 'Escalope de ternera', 'Ensalada básica con zanahoria rallada','Mandarina', 'Pan de trigo integral'], ['1 1/2 cucharón', '1 bistec mediano', '1 guarnición', '1 grande', '1 rodaja'], [200, 100, 120, 180, 20], 785.9, 19, 34, 47)
+        @d2 = Dieta.new('ALMUERZO', 40,  ['Macarrones con salsa de tomate y queso parmesano', 'Escalope de ternera', 'Ensalada básica con zanahoria rallada','Mandarina', 'Pan de trigo integral'], ['1 1/2 cucharón', '1 bistec mediano', '1 guarnición', '1 grande', '1 rodaja'], [200, 100, 120, 180, 20], 785.9, 19, 34, 47)
         @d4 = LList.new(1)
         @d5=LList.new(@d1)
 
     end
+    
+
+    describe "List_Functions" do
+        it "popb numbers" do
+        @d4.pushe(2)
+        @d4.pushe(3)
+        @d4.pushe(4)
+        @d4.pushe(5)
+        expect(@d4.popb).to eq(1)
+        expect(@d4.popb).to eq(2)
+        expect(@d4.popb).to eq(3)
+        expect(@d4.popb).to eq(4)
+        expect(@d4.popb).to eq(5)
+      end
+      
+      it "pope numbers" do
+        @d4.pushe(2)
+        @d4.pushe(3)
+        @d4.pushe(4)
+        @d4.pushe(5)
+        expect(@d4.pope).to eq(5)
+        expect(@d4.pope).to eq(4)
+        expect(@d4.pope).to eq(3)
+        expect(@d4.pope).to eq(2)
+        expect(@d4.pope).to eq(1)
+      end
+      
+      it "popb and pope numbers" do
+        @d4.pushe(2)
+        @d4.pushe(3)
+        @d4.pushe(4)
+        @d4.pushe(5)
+        expect(@d4.popb).to eq(1)
+        expect(@d4.pope).to eq(5)
+        expect(@d4.popb).to eq(2)
+        expect(@d4.pope).to eq(4)
+        expect(@d4.popb).to eq(3)
+
+      end
+      
+      it "nil numbers" do
+        expect(@d4.popb).to eq(1)
+        expect(@d4.popb).to eq(nil)
+      end
+      
+      it "popb Dieta" do
+        @d5.pushb(@d2)
+        expect(@d5.popb).to eq(@d2)
+        expect(@d5.popb).to eq(@d1)
+      end
+      
+      it "pope Dieta" do
+        @d5.pushb(@d2)
+        expect(@d5.pope).to eq(@d1)
+        expect(@d5.pope).to eq(@d2)
+      end
+      
+      it "popb and pope Dieta" do
+        @d5.pushb(@d2)
+        @d5.pushb(@d1)
+        @d5.pushb(@d2)
+        expect(@d5.popb).to eq(@d2)
+        expect(@d5.pope).to eq(@d1)
+        expect(@d5.popb).to eq(@d1)
+        expect(@d5.pope).to eq(@d2)
+      end
+      
+      it "Dieta nil" do
+        expect(@d4.popb).to eq(@d1)
+        expect(@d4.popb).to eq(nil)
+      end
+      
+      
+      end
   end
