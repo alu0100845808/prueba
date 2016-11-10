4e08e9eff4207314ed307a1372a4dff120efbab9
Alejandro Carrillo <alu0100845808@ull.edu.es>
2016-11-10 17:32:37 +0000

    Rake finalizado y empezando pruebas

--- Dieta/spec/Dieta_spec.rb
+++ Dieta/spec/Dieta_spec.rb
@@ -1,5 +1,6 @@
 require "spec_helper"
-
+require '/home/ubuntu/workspace/p7/Dieta/lib/dieta'
+require '/home/ubuntu/workspace/p7/Dieta/lib/LList'
 describe Dieta do
   it "has a version number" do
     expect(Dieta::VERSION).not_to be nil
