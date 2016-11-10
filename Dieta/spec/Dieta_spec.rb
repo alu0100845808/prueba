d1a7f034bfa7891600a337bc9228ccc44ebae6aa
Alejandro Carrillo <alu0100845808@ull.edu.es>
2016-11-10 17:36:14 +0000

    Empezando pruebas

--- Dieta/spec/Dieta_spec.rb
+++ Dieta/spec/Dieta_spec.rb
@@ -1,12 +1,13 @@
 require "spec_helper"
 require '/home/ubuntu/workspace/p7/Dieta/lib/dieta'
 require '/home/ubuntu/workspace/p7/Dieta/lib/LList'
-describe Dieta do
-  it "has a version number" do
-    expect(Dieta::VERSION).not_to be nil
-  end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe Dieta do
+    before :each do
+        @d1 = Dieta.new('ALMUERZO', 30,  ['Macarrones con salsa de tomate y queso parmesano', 'Escalope de ternera', 'Ensalada básica con zanahoria rallada','Mandarina', 'Pan de trigo integral'], ['1 1/2 cucharón', '1 bistec mediano', '1 guarnición', '1 grande', '1 rodaja'], [200, 100, 120, 180, 20], 785.9, 19, 34, 47)
+        @d2 = Dieta.new('ALMUERZO', 30,  ['Macarrones con salsa de tomate y queso parmesano', 'Escalope de ternera', 'Ensalada básica con zanahoria rallada','Mandarina', 'Pan de trigo integral'], ['1 1/2 cucharón', '1 bistec mediano', '1 guarnición', '1 grande', '1 rodaja'], [200, 100, 120, 180, 20], 785.9, 19, 34, 47)
+        @d4 = LList.new(1)
+        @d5=LList.new(@d1)
+
+    end
   end
-end
