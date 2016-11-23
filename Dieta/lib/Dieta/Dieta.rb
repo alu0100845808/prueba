f0992308b3b381eb17239c56cfa6bf5aa60c7bd6
Alejandro Carrillo <alu0100845808@ull.edu.es>
2016-11-23 13:11:04 +0000

    Comparable

--- Dieta/lib/Dieta/Dieta.rb
+++ Dieta/lib/Dieta/Dieta.rb
@@ -1,4 +1,5 @@
 class Dieta
+    include Comparable
     attr_accessor :titulo,:porcmenu,:desc_platos,:por_platos,:gr_platos,:vct,:porcprot,:porcgrasas,:porchidratos
     def initialize(titulo,porcmenu,desc_platos,por_platos,gr_platos,vct,porcprot,porcgrasas,porchidratos)
         @titulo=titulo
@@ -11,4 +12,13 @@ class Dieta
         @porcgrasas=porcgrasas
         @porchidratos=porchidratos
     end
+    
+    def <=> (other)
+        if(@gr_platos == other.gr_platos)
+            @porcprot <=> other.porcprot
+        else
+            @gr_platos <=> other.gr_platos
+        end
+    end
+    
 end
